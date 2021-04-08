881d78f2cd179e8157d204dfa683ad0e1bc9bb03
Taylor Coffman <CoffmanTaylor96+git@gmail.com>
2021-04-07 18:07:53 -0700

    basic cache funcitality for single transition system

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    hash::Hash,
+    marker::PhantomData,
+};
+
 pub trait PartialStateTransition<S, Args> {
     type Striped;
 
@@ -6,21 +12,67 @@ pub trait PartialStateTransition<S, Args> {
     fn partial_call(input: Self::Striped, args: Args) -> Self::Striped;
 
     fn merge(input: S, result: Self::Striped) -> S;
+}
 
-    fn call(input: S, args: Args) -> S
+pub fn apply_transition<T, S, Args>(start: S, args: Args) -> S
+where
+    T: PartialStateTransition<S, Args>,
+    S: Clone,
+{
+    let stripped = T::strip(start.clone());
+    T::merge(start, T::partial_call(stripped, args))
+}
+
+pub struct TransitionCache<T, S, Args>
+where
+    T: PartialStateTransition<S, Args>,
+{
+    transition: PhantomData<T>,
+    cache: HashMap<(T::Striped, Args), T::Striped>,
+}
+
+impl<T, S, Args> TransitionCache<T, S, Args>
+where
+    T: PartialStateTransition<S, Args>,
+{
+    pub fn new() -> Self {
+        TransitionCache {
+            transition: PhantomData,
+            cache: HashMap::new(),
+        }
+    }
+
+    pub fn apply_transition(&mut self, start: S, args: Args) -> S
     where
         S: Clone,
+        T::Striped: Clone + Eq + Hash,
+        Args: Clone + Eq + Hash,
     {
-        let striped = Self::strip(input.clone());
-        Self::merge(input, Self::partial_call(striped, args))
+        let striped = T::strip(start.clone());
+
+        // Check if we already have cached this transition.
+        let res_unmerged = match self.cache.entry((striped.clone(), args.clone())) {
+            Entry::Occupied(e) => e.get().clone(),
+            Entry::Vacant(e) => {
+                // We do not already have it. Compute the transition and add it to the map.
+                let res = T::partial_call(striped, args);
+                e.insert(res.clone());
+                res
+            }
+        };
+
+        T::merge(start, res_unmerged)
     }
 }
 
 #[cfg(test)]
 mod hand_written {
+
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
     use super::*;
 
-    #[derive(Clone, PartialEq, Eq, Debug)]
+    #[derive(Clone, PartialEq, Eq, Debug, Hash)]
     struct Example {
         field_usize: usize,
         field_bool: bool,
@@ -35,8 +87,7 @@ mod hand_written {
         }
     }
 
-    #[derive(Debug, Hash, PartialEq, Eq)]
-    struct IsEven;
+    enum IsEven {}
 
     impl PartialStateTransition<Example, ()> for IsEven {
         type Striped = Example;
@@ -59,173 +110,55 @@ mod hand_written {
     fn direct_call_on_is_even() {
         let start = Example::new(42, false);
 
-        assert_eq!(Example::new(42, true), IsEven::call(start, ()));
+        assert_eq!(
+            Example::new(42, true),
+            apply_transition::<IsEven, _, _>(start, ())
+        );
+    }
+
+    #[test]
+    fn can_apply_transition_on_cache() {
+        let mut cache = TransitionCache::<IsEven, _, _>::new();
+
+        let start = Example::new(42, false);
+
+        let res = cache.apply_transition(start, ());
+
+        assert_eq!(Example::new(42, true), res);
+    }
+
+    #[test]
+    fn cached_transition_is_only_called_once() {
+        static COUNT: AtomicUsize = AtomicUsize::new(0);
+
+        enum IsEven {}
+
+        impl PartialStateTransition<Example, ()> for IsEven {
+            type Striped = Example;
+
+            fn strip(input: Example) -> Self::Striped {
+                input
+            }
+
+            fn partial_call(mut input: Self::Striped, _args: ()) -> Self::Striped {
+                COUNT.fetch_add(1, Ordering::SeqCst);
+                input.field_bool = input.field_usize % 2 == 0;
+                input
+            }
+
+            fn merge(_input: Example, result: Self::Striped) -> Example {
+                result
+            }
+        }
+
+        let mut cache = TransitionCache::<IsEven, _, _>::new();
+
+        let start = Example::new(42, false);
+
+        cache.apply_transition(start.clone(), ());
+        let res = cache.apply_transition(start, ());
+
+        assert_eq!(Example::new(42, true), res);
+        assert_eq!(1, COUNT.load(Ordering::SeqCst));
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-
-//     use core::panic;
-//     use std::collections::HashMap;
-
-//     #[derive(Clone, Debug, PartialEq, Eq)]
-//     struct ExampleComposition {
-//         field_usize: Handle<usize>,
-//         field_bool: Handle<bool>,
-//         field_char: Handle<char>,
-//     }
-
-//     #[allow(non_camel_case_types)]
-//     #[derive(Clone, PartialEq, Eq, Hash)]
-//     struct _ExampleComposition_example_is_even {
-//         field_usize: Handle<usize>,
-//         field_bool: Handle<bool>,
-//     }
-
-//     #[allow(non_camel_case_types)]
-//     #[derive(Clone, PartialEq, Eq, Hash)]
-//     struct _ExampleComposition_example_not {
-//         field_bool: Handle<bool>,
-//     }
-
-//     #[allow(non_camel_case_types)]
-//     #[derive(Clone, Eq, PartialEq, Hash)]
-//     enum _ExampleComposition_Transitions {
-//         _ExampleComposition_example_is_even(_ExampleComposition_example_is_even),
-//         _ExampleComposition_example_not(_ExampleComposition_example_not),
-//     }
-
-//     impl _ExampleComposition_Transitions {
-//         fn to_is_even(self) -> _ExampleComposition_example_is_even {
-//             match self {
-//                 _ExampleComposition_Transitions::_ExampleComposition_example_is_even(v) => v,
-//                 _ => panic!("Tried to convert to is_even on non is_even"),
-//             }
-//         }
-//         fn to_not(self) -> _ExampleComposition_example_not {
-//             match self {
-//                 _ExampleComposition_Transitions::_ExampleComposition_example_not(v) => v,
-//                 _ => panic!("Tried to convert to not on non not"),
-//             }
-//         }
-//     }
-
-//     fn example_is_even(
-//         input: &_ExampleComposition_example_is_even,
-//     ) -> _ExampleComposition_example_is_even {
-//         let mut output = input.clone();
-
-//         *output.field_bool.get_mut() = *input.field_usize % 2 == 0;
-
-//         output
-//     }
-
-//     fn example_not(input: &_ExampleComposition_example_not) -> _ExampleComposition_example_not {
-//         let mut output = input.clone();
-
-//         *output.field_bool.get_mut() = !*input.field_bool;
-
-//         output
-//     }
-
-//     fn _strip_example_is_even(start: &ExampleComposition) -> _ExampleComposition_example_is_even {
-//         _ExampleComposition_example_is_even {
-//             field_bool: start.field_bool.clone(),
-//             field_usize: start.field_usize.clone(),
-//         }
-//     }
-
-//     fn _strip_example_not(start: &ExampleComposition) -> _ExampleComposition_example_not {
-//         _ExampleComposition_example_not {
-//             field_bool: start.field_bool.clone(),
-//         }
-//     }
-
-//     fn _merge_example_is_even(
-//         result: _ExampleComposition_example_is_even,
-//         mut start: ExampleComposition,
-//     ) -> ExampleComposition {
-//         start.field_usize = result.field_usize;
-//         start.field_bool = result.field_bool;
-
-//         start
-//     }
-
-//     fn _merge_example_not(
-//         result: _ExampleComposition_example_not,
-//         mut start: ExampleComposition,
-//     ) -> ExampleComposition {
-//         start.field_bool = result.field_bool;
-
-//         start
-//     }
-
-//     fn call_example_is_even(
-//         input: ExampleComposition,
-//         memo_cache: &mut HashMap<_ExampleComposition_Transitions, _ExampleComposition_Transitions>,
-//     ) -> ExampleComposition {
-//         let striped = _strip_example_is_even(&input);
-
-//         // Check if we already have a result for that state.
-//         if let Some(result) = memo_cache.get(
-//             &_ExampleComposition_Transitions::_ExampleComposition_example_is_even(striped.clone()),
-//         ) {
-//             _merge_example_is_even(result.clone().to_is_even(), input)
-//         } else {
-//             let result_unmerged = example_is_even(&striped);
-//             memo_cache.insert(
-//                 _ExampleComposition_Transitions::_ExampleComposition_example_is_even(striped),
-//                 _ExampleComposition_Transitions::_ExampleComposition_example_is_even(
-//                     result_unmerged.clone(),
-//                 ),
-//             );
-//             _merge_example_is_even(result_unmerged, input)
-//         }
-//     }
-
-//     fn call_example_not(
-//         input: ExampleComposition,
-//         memo_cache: &mut HashMap<_ExampleComposition_Transitions, _ExampleComposition_Transitions>,
-//     ) -> ExampleComposition {
-//         let striped = _strip_example_not(&input);
-
-//         // Check if we already have a result for that state.
-//         if let Some(result) = memo_cache
-//             .get(&_ExampleComposition_Transitions::_ExampleComposition_example_not(striped.clone()))
-//         {
-//             _merge_example_not(result.clone().to_not(), input)
-//         } else {
-//             let result_unmerged = example_not(&striped);
-//             memo_cache.insert(
-//                 _ExampleComposition_Transitions::_ExampleComposition_example_not(striped),
-//                 _ExampleComposition_Transitions::_ExampleComposition_example_not(
-//                     result_unmerged.clone(),
-//                 ),
-//             );
-//             _merge_example_not(result_unmerged, input)
-//         }
-//     }
-
-//     #[test]
-//     fn can_strip_and_merge() {
-//         let start = ExampleComposition {
-//             field_usize: Handle::new_unifying_state(42),
-//             field_bool: Handle::new_unifying_state(false),
-//             field_char: Handle::new_unifying_state('a'),
-//         };
-
-//         let mut memo_cache = HashMap::new();
-
-//         let result = call_example_is_even(start.clone(), &mut memo_cache);
-
-//         assert_eq!(
-//             ExampleComposition {
-//                 field_usize: start.field_usize.clone(),
-//                 field_char: start.field_char.clone(),
-//                 field_bool: start.field_bool.add_unifying_state(true),
-//             },
-//             result
-//         );
-//     }
-// }
